ade388a94af7d6ff56818387738fc2ddea5c8c77
Robin Millette <robin@millette.info>
2018-08-12 16:50:08 -0400

    Fail on browser.

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 'use strict'
 
+if (typeof window !== 'undefined') {
+  throw new Error('Browser not supported')
+  /*
+  module.exports = {
+    ret: {},
+    Doit: DoitImp = () => { throw new Error('Browser not supported') }
+  }
+  return
+  */
+}
+
 // npm
 const { fromUrl } = require('hosted-git-info')
 
+// core
+const { readFileSync } = require('fs')
+const resolveCwd = require('path').resolve.bind(null, process.cwd())
+
 // self
-const { name, version, repository } = require('./package.json')
+const { name, version, repository } = require(resolveCwd('package.json'))
 const versionTag = `v${version}`
 const ret = { name, version, versionTag, repository }
-if (typeof window === 'undefined') {
-  // npm
-  const { readFileSync } = require('fs')
-  try {
-    ret.headHash = readFileSync(
-      `.git/${readFileSync('.git/HEAD', 'utf-8')
-        .slice(5)
-        .trim()}`,
-      'utf-8'
-    ).trim()
-    ret.versionHash = readFileSync(`.git/refs/tags/${versionTag}`, 'utf-8').trim()
-    ret.dev = ret.versionHash !== ret.headHash
-  } catch (e) {
-    if (!ret.versionHash) {
-      delete ret.versionTag
-    }
-  }
-}
 
 class Doit {
   constructor (u, v) {
@@ -53,4 +50,25 @@ class Doit {
   }
 }
 
-module.exports = { ret, Doit }
+try {
+  const p1 = resolveCwd('.git/HEAD')
+  const p2 = resolveCwd(
+      `.git/${readFileSync(p1, 'utf-8')
+        .slice(5)
+        .trim()}`
+  )
+  ret.headHash = readFileSync(p2, 'utf-8').trim()
+  const p3 = resolveCwd('.git/refs/tags', versionTag)
+
+  ret.versionHash = readFileSync(p3, 'utf-8').trim()
+  ret.dev = ret.versionHash !== ret.headHash
+} catch (e) {
+  if (!ret.versionHash) {
+    delete ret.versionTag
+  }
+}
+
+module.exports = {
+  ret,
+  Doit
+}
